6ea562d0bda9f90a2477205fcfc7eb09d9b910f2
Louis <70532216+l0uisgrange@users.noreply.github.com>
2025-05-08 18:48:18 +0200

    Start

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
 	import '../app.css';
+	import github from '$lib/github-mark.svg';
+	import circuit from '$lib/main.svg';
 
 	let { children } = $props();
 </script>
 
+<div class="py-4">
+	<div
+		class="mx-auto flex max-w-7xl items-center justify-between px-4 py-2.5 text-lg font-medium"
+	>
+		Circuitor
+		<img src={github} alt="circuit" class="size-7 opacity-50" />
+	</div>
+</div>
 {@render children()}
+<div></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import circuit from "$lib/main.svg"
+</script>
+
+<div class="min-h-screen w-screen bg-neutral-700 text-center text-white flex flex-col justify-center items-center">
+	<h1 class="text-7xl font-bold font-stretch-expanded max-w-3xl">Draw electronic circuits like a champion</h1>
+	<div>
+		<img src={circuit} alt="circuit" />
+	</div>
+	<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
