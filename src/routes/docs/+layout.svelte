90628d6c6b48a8885f5471e5a47ab9dbcc965272
Louis <70532216+l0uisgrange@users.noreply.github.com>
2025-05-11 12:12:21 +0200

    Unified installation

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -12,7 +12,8 @@
 	class="dark:border-neutral-border flex w-full cursor-pointer items-center justify-between border-b border-neutral-200 px-5 py-2.5 md:hidden"
 >
 	Menu
-	<span class="icon-[hugeicons--arrow-down-01] size-6"></span>
+	<span class="icon-[hugeicons--arrow-down-01] size-6 transition {menuopen && 'rotate-180'}"
+	></span>
 </button>
 <div class="relative md:flex">
 	<div
--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -28,10 +28,10 @@
 	<main class="min-h-dvh flex-auto p-5 pt-10 md:px-8 md:py-10">
 		<h1>Getting started</h1>
 		<p class="max-w-2xl text-lg">
-			Circuitor is easy to set up and works out of the box with <a
+			Circuitor is very easy to set up and works out of the box with the <a
 				target="_blank"
 				href="https://typst.app/universe">Typst Universe</a
-			>. There are two main ways to start using the library, depending on your workflow.
+			> registry.
 		</p>
 		<Notice type="danger" title="Upcoming Typst release">
 			Circuitor will be released in Typst Universe on <span class="font-medium"
@@ -40,38 +40,12 @@
 			<a target="_blank" href="https://github.com/l0uisgrange/circuitor">GitHub repository</a
 			>.
 		</Notice>
-		<h2>Typst Universe</h2>
+		<h2>Installation</h2>
 		<p>
-			If you're working in the online Typst app, you can import Circuitor directly with a
-			single line.
+			Whether you're using the Typst online app or running Typst locally on your computer, you
+			can start using Circuitor by adding the following line at the top of your Typst file.
 		</p>
 		<Code content={'#import "@preview/circuitor:0.1.0"'} />
-		<h2 class="mt-3">Local package</h2>
-		<p class="mb-4!">
-			If you prefer to work offline or want to explore the source code, you can also use
-			Circuitor by downloading it locally from Typst Universe registry.
-		</p>
-		<a
-			target="_blank"
-			href="https://packages.typst.org/preview/circuitor-0.1.0.tar.gz"
-			class="dark:border-neutral-border flex max-w-max items-center gap-2 rounded-full border border-neutral-200 px-5 py-2! font-medium text-current! decoration-0 transition hover:border-black dark:hover:border-white"
-		>
-			<span class="icon-[hugeicons--download-02] size-5.5"></span>
-			Download
-		</a>
-		<ul class="ms-7 mt-4 list-outside list-decimal">
-			<li>Download the package to your computer.</li>
-			<li>Uncompress it</li>
-			<li>
-				In your Typst project, import the <span class="font-mono">lib.typ</span> file located
-				in the src folder:
-			</li>
-		</ul>
-		<Code content={'#import "PATH_TO_CIRCUITOR/src/lib.typ"'} />
-		<p>
-			Make sure the path reflects the actual location of the downloaded folder relative to
-			your .typ file.
-		</p>
 		<h2>Report bugs</h2>
 		<p>I actively maintain Circuitor and greatly value community feedback.</p>
 		<p>
@@ -85,11 +59,15 @@
 				>discussion</a
 			> on the GitHub repository.
 		</p>
-		<h2>Minimal starting point</h2>
+		<h2>Create your first circuit</h2>
 		<p>
-			To get started with Circuitor, here’s a minimal working example showing how to draw a
+			To get started with Circuitor, here's a minimal working example showing how to draw a
 			simple circuit with just a few lines of code.
 		</p>
+		<Notice type="tip" title="Tip"
+			>It is recommended to import circuitor component inside your canvas, as some circuitor
+			functions override some basic Typst elements.</Notice
+		>
 		<Code
 			content={'#import "@preview/circuitor:0.1.0"\n' +
 				'\n' +
